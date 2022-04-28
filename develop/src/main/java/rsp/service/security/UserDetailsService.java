4baa8b1f3bb3a805c13d21252a047beb1b574e22
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-28 20:25:39 +0200

    feat(user): add UserDetails and UserDetailsService

---
+++ develop/src/main/java/rsp/service/security/UserDetailsService.java
@@ -0,0 +1,27 @@
+package rsp.service.security;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import rsp.model.User;
+import rsp.repo.UserRepo;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    final private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(
+                       () -> new UsernameNotFoundException(String.format("User '%s' not found.", username))
+                );
+
+        return new rsp.security.UserDetails(user);
+    }
+}
