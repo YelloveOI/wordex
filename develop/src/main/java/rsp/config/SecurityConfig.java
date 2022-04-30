1e6076a8f808c595a2d4d57bad65eb8441fe8ff5
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-30 10:57:46 +0200

    feat(cors): add cors configuration

--- develop/src/main/java/rsp/config/SecurityConfig.java
+++ develop/src/main/java/rsp/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package rsp.config;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,8 +14,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import rsp.security.filters.JwtAuthorizationFilter;
 import rsp.security.service.UserDetailsService;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
+        http.cors()
+            .and()
             .csrf().disable()
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, "/auth/*").permitAll()
@@ -45,5 +52,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        val configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Content-Type", "X-Requested-With", "accept,Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization", "Cache-Control", "Access-Control-Allow-Origin"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        val source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
